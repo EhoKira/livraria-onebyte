25896081df5031995d02f6e269fe94edac5110e1
EhoKira <eduardo57izidorio@gmail.com>
2024-09-30 10:57:09 -0400

    Mudando mensagem

--- Server/controladores/livro.js
+++ Server/controladores/livro.js
@@ -1,6 +1,6 @@
 function getLivros(req, res) {
     try{
-        res.send('Olá mundo! Livraria OneByte')
+        res.send('Seja Bem-Vindo à Livraria OneByte')
     } catch (error) {
         res.status(500)
         res.send(error.message)
