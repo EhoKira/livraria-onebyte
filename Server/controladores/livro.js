183f32b8e136a80a2611bc447fdafacf16a1aa8d
EhoKira <eduardo57izidorio@gmail.com>
2024-10-01 15:42:48 -0400

    Criando a função PATCH

--- Server/controladores/livro.js
+++ Server/controladores/livro.js
@@ -1,4 +1,4 @@
-const { getTodosLivros, getLivroPorId, insereLivro } = require("../servicos/livro")
+const { getTodosLivros, getLivroPorId, insereLivro, modificaLivro } = require("../servicos/livro")
 
 function getLivros(req, res) {
     try{
@@ -29,12 +29,26 @@ function postLivro(req, res) {
         res.send("Livro inserido com sucesso!")
     } catch(error) {
         res.status(500)
-        res.send(send.message)
+        res.send(error.message)
+    }
+}
+
+function patchLivro(req, res) {
+    try{
+        const id = req.params.id
+        const body = req.body
+
+        modificaLivro(body, id)
+        res.send("Item modificado com sucesso!")
+    } catch(error) {
+        res.status(500)
+        res.send(error.message)
     }
 }
 
 module.exports = {
     getLivros,
     getLivro,
-    postLivro
+    postLivro,
+    patchLivro
 }
