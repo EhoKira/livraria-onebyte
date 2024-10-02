03d45d0daf72cc1b7ad768df422f7e60b5fc2ce1
EhoKira <eduardo57izidorio@gmail.com>
2024-10-02 15:06:57 -0400

    Arquivos atualizados com todas as requisições de favoritos funcionando

--- Server/servicos/favorito.js
+++ Server/servicos/favorito.js
@@ -15,7 +15,7 @@ function insereFavorito(id) {
     const livros = JSON.parse( fs.readFileSync("livros.json"))
     const favoritos = JSON.parse( fs.readFileSync("favoritos.json"))
 
-    const livroInserido = livro.find( livro = livro.id === id)
+    const livroInserido = livros.find( livro => livro.id === id)
     const novaListaDeLivrosFavoritos = [ ...favoritos, livroInserido]
     fs.writeFileSync("favoritos.json", JSON.stringify(novaListaDeLivrosFavoritos))
 }
