a149faa4322478da16e5c9be01bebdfe82f0c081
EhoKira <eduardo57izidorio@gmail.com>
2024-10-01 21:25:23 -0400

    Criando conexão entre backend e frontend

--- Client/src/componentes/Pesquisa/dadosPesquisa.js
+++ Client/src/componentes/Pesquisa/dadosPesquisa.js
@@ -1,5 +1,6 @@
 import livro from '../../imagens/livro.png'
 
+
 export const livros = [
     {nome: 'Liderança em Design', id: 1, src: livro},
     {nome: 'JavaScript Assertivo', id: 2, src:livro},
