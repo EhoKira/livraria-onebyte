5bc78601a74a1251ab9d963a1964565b76af0cda
EhoKira <eduardo57izidorio@gmail.com>
2024-10-02 01:32:52 -0400

    Adicionando função de insert e delete, criando rota

--- Client/src/rotas/Favoritos.js
+++ Client/src/rotas/Favoritos.js
@@ -1,5 +1,7 @@
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
-import Pesquisa from '../componentes/Pesquisa';
+import { deleteFavorito, getFavoritos } from '../servicos/favoritos';
+import livroImg from '../imagens/livro.png'
 
 const AppContainer = styled.div`
       width: 100vw;
@@ -7,10 +9,68 @@ const AppContainer = styled.div`
       background-image: linear-gradient(90deg, #FFA07A 35%, #FA8072);
 `
 
+const ResultadoContainer = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+`
+
+const Resultado = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 20px 0;
+    cursor: pointer;
+    text-align: center;
+    padding: 0 100px;
+    p {
+        width: 200px;
+        color: #FFF;
+    }
+    img {
+        width: 100px;
+    }
+    &:hover {
+        border: 1px solid white;
+    }
+`
+
+const Titulo = styled.h2`
+    color: #FFF;
+    font-size: 36px;
+    text-align: center;
+    width: 100%;
+    padding-top: 35px
+`
+
 function Favoritos() {
+  const [favoritos, setFavoritos] = useState([])
+
+  async function fetchFavoritos() {
+    const favoritosDaAPI = await getFavoritos()
+    setFavoritos(favoritosDaAPI)
+  }
+
+  async function deletarFavorito(id) {
+    await deleteFavorito(id)
+    await fetchFavoritos()
+    alert(`Livro de id:${id} deletado!`)
+}
+
+  useEffect(() => {
+    fetchFavoritos([])
+  }, [])
+
   return (
     <AppContainer>
-      <Pesquisa />
+        <Titulo>Aqui estão seus Livros Favoritos</Titulo>
+          <ResultadoContainer>
+      {favoritos.map( favorito => (
+        <Resultado onClick={() => deletarFavorito(favorito.id)}>
+          <img src={livroImg}/>
+          <p>{favorito.nome}</p>
+        </Resultado>))}
+      </ResultadoContainer>
     </AppContainer>
   );
 }
